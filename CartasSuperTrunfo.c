fa055c40d57b6f8c63be086e10a77dcfd305fde9
Matheus Ferraz <contatomatheus.oferraz@gmail.com>
2025-08-12 19:01:25 +0000

    atualizações conforme feedback da IA

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -73,11 +73,32 @@ int main() {
     scanf("%d", &pontos_turisticos[1]);
     limpar_buffer();
 
+    // Cálculos
     float densidade_1 = (float)populacao[0] / kms_quadrados[0]; 
+    if (kms_quadrados[0] != 0) {
+        densidade_1 = (float)populacao[0] / kms_quadrados[0];
+    } else {
+        densidade_1 = 0.0F;
+    };
     float calculo_pib_1 = (pib[0] * 1000000000.0) / (float)populacao[0];
+    if (populacao[0] != 0) {
+        calculo_pib_1 = (pib[0] * 1000000000.0) / (float)populacao[0];
+    } else {
+        calculo_pib_1 = 0.0F;
+    }
 
     float densidade_2 = (float)populacao[1] / kms_quadrados[1]; 
+    if (kms_quadrados[1] != 0) {
+        densidade_2 = (float)populacao[1] / kms_quadrados[1];
+    } else {
+        densidade_2 = 0.0F;
+    };
     float calculo_pib_2 = (pib[1] * 1000000000.0) / (float)populacao[1];
+    if (populacao[1] != 0) {
+        calculo_pib_2 = (pib[1] * 1000000000.0) / (float)populacao[1];
+    } else {
+        calculo_pib_2 = 0.0F;
+    }
 
     // Impressão
     printf("\n\n===== Informações das Cidades =====\n");
@@ -109,15 +130,20 @@ int main() {
     // Carta Vencedora
     printf("\n--- Comparação das cartas ---\n");
 
-    int populacaoResultado = populacao[0] > populacao[1] ? 1 : 2;
-    int kms_quadradosResultado = kms_quadrados[0] > kms_quadrados[1] ? 1 : 2;
-    int pibResultado = pib[0] > pib[1] ? 1 : 2;
-    int pontos_turisticosResultado = pontos_turisticos[0] > pontos_turisticos[1] ? 1 : 2;
-    int densidade_resultado = densidade_1 < densidade_2 ? 1 : 2;
-    int pib_per_capita_resultado = calculo_pib_1 > calculo_pib_2 ? 1 : 2;
+    printf("Comparando as cartas...\n");
 
-    float superPoder1 = populacao[0] + kms_quadrados[0] + pib[0] + pontos_turisticos[0] + calculo_pib_1 - densidade_1;
-    float superPoder2 = populacao[1] + kms_quadrados[1] + pib[1] + pontos_turisticos[1] + calculo_pib_2 - densidade_2;
+    int populacaoResultado = (populacao[0] > populacao[1]) ? 1 : 2;
+    int kms_quadradosResultado = (kms_quadrados[0] > kms_quadrados[1]) ? 1 : 2;
+    int pibResultado = (pib[0] > pib[1]) ? 1 : 2;
+    int pontos_turisticosResultado = (pontos_turisticos[0] > pontos_turisticos[1]) ? 1 : 2;
+    int densidade_resultado = (densidade_1 < densidade_2) ? 1 : 2;
+    int pib_per_capita_resultado = (calculo_pib_1 > calculo_pib_2) ? 1 : 2;
+
+    float inversoDensidade1 = (densidade_1 != 0) ? (1.0F / densidade_1) : 0.0F;
+    float inversoDensidade2 = (densidade_2 != 0) ? (1.0F / densidade_2) : 0.0F;
+
+    float superPoder1 = (float)populacao[0] + kms_quadrados[0] + pib[0] + (float)pontos_turisticos[0] + calculo_pib_1 + inversoDensidade1;
+    float superPoder2 = (float)populacao[1] + kms_quadrados[1] + pib[1] + (float)pontos_turisticos[1] + calculo_pib_2 + inversoDensidade2;
 
     printf("População: Carta %d venceu\n", populacaoResultado);
     printf("Área: Carta %d venceu\n", kms_quadradosResultado);
